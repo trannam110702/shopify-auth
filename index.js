939f94f7959bb3e200a853e049015fa121d3d123
Ethan <namtlv@avadagroup.com>
2025-09-09 18:03:21 +0700

    wip

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const Koa = require('koa');
-const render = require('koa-ejs');
-const path = require('path');
+const Koa = require("koa");
+const render = require("koa-ejs");
+const path = require("path");
 
 const app = new Koa();
 
 render(app, {
-    root: path.join(__dirname, 'view'),
-    layout: false,
-    viewExt: 'html',
-    cache: false,
-    debug: false
+  root: path.join(__dirname, "view"),
+  layout: false,
+  viewExt: "html",
+  cache: false,
+  debug: false,
 });
 
 app.use(async function (ctx) {
-    console.log('received a request', ctx.url);
-    await ctx.render('template');
+  console.log("received a request", ctx.url, ctx.query);
+
+  await ctx.render("template");
 });
 
 app.listen(3000);
